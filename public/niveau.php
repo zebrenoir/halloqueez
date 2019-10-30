f482e9b62facb0f6ae8d11faa44093d9bb2c7420
christian <christian.coley@hotmail.fr>
2019-10-30 16:57:25 +0100

    apparition des niveaux

---
+++ public/niveau.php
@@ -0,0 +1,28 @@
+<?php require_once "header.html"?>
+<?php
+
+
+
+
+?>
+
+
+<h1> level</h1>
+<ul class="levels">
+    <li class="lvl">
+        <p class="number">1</p>
+        <img class="lock" src="../src/image/cadenas.png">
+    </li>
+
+    <li class="lvl">
+        <p class="number">2</p>
+        <img class="lock" src="../src/image/cadenas.png">
+    </li>
+
+
+</ul>
+
+
+</body>
+</html>
+
