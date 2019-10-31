c656c15832f3def1dcfa688275e6589149f5ee0a
Damien Baudet <damienbaudet@free.fr>
2019-10-31 13:41:42 +0100

    css niveau

--- public/niveau.php
+++ public/niveau.php
@@ -6,18 +6,18 @@ require_once "phpNiveau.php";
 
 ?>
 <body class="body_niveau">
-<h1 class="title_pageNiveau"> level</h1>
+<h1 class="title_pageNiveau">Select the level</h1>
 
 <ul class="levels">
     <li class="lvl">
-        <a <?php echo hiddenLien1() ?> class="number">1</a>
+            <a <?php echo hiddenLien1() ?> class="number">1</a>
 
-        <img <?php echo hidden1();?>class="lock" src="../src/image/cadenas.png">
+            <img <?php echo hidden1(); ?>class="lock" src="../src/image/cadenas.png">
     </li>
 
     <li class="lvl">
-        <a <?php echo hiddenLien2() ?> class="number">2</a>
-        <img <?php echo hidden2();?> class="lock" src="../src/image/cadenas.png">
+            <a <?php echo hiddenLien2() ?> class="number">2</a>
+            <img <?php echo hidden2(); ?> class="lock" src="../src/image/cadenas.png">
     </li>
 
 
@@ -25,5 +25,5 @@ require_once "phpNiveau.php";
 
 </body>
 
-<?php require_once "footer.html"?>
+<?php require_once "footer.html" ?>
 
