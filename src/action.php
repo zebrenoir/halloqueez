6eb252d261b4a63ca68b997d9b07cab8717a42c6
Damien Baudet <damienbaudet@free.fr>
2019-10-31 11:32:26 +0100

    update cadenas

--- src/action.php
+++ src/action.php
@@ -3,11 +3,12 @@
 require_once '../connec2.php';
 $step = 1;
 $goodAnswer = 0;
+$score = $_SESSION["username"];
 
 if (!empty($_POST)) {
     $step = $_POST['step'];
     $goodAnswer = $_POST['goodAnswer'];
-    if ($step <= 10) {
+    if ($step < 10) {
         $step += 1;
         if (!empty($_POST['q'])) {
             $goodAnswer += 1;
@@ -15,10 +16,12 @@ if (!empty($_POST)) {
     }
     else {
         $pdo = new PDO(DSN, USER, PASS);
-        $query = 'UPDATE user SET level1=:goodAnswer WHERE username=$_SESSION["username"]';
+        $query = 'UPDATE user SET level1=:goodAnswer WHERE username=:score';
         $prep = $pdo->prepare($query);
 
         $prep->bindValue(':goodAnswer', $goodAnswer, PDO::PARAM_STR);
+        $prep->bindValue(':score', $score, PDO::PARAM_STR);
+
         $prep->execute();
 
         $article = $prep->fetchAll();
